597eb5087d32c35a2f939899c2549a1d61442102
Javier0626 <jadanaga@hotmail.com>
2024-08-17 17:25:24 -0500

    Iniciando Proyecto Frontend

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img
+          src='https://cdn.pixabay.com/photo/2024/03/26/11/57/e-commerce-8656646_960_720.jpg'
+          className='App-logo'
+          alt='logo'
+        />
+        <p>Bienvenidos a sistema de gestion para envios nacionales.</p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Aprende mas!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
